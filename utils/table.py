e902bc5224ba83329770347c5897623790dcac6e
Andrea Haritcalde <andrea.haritcalde@gmail.com>
2024-02-08 10:56:11 +0100

    Seat up Seat class init and methods

--- utils/table.py
+++ utils/table.py
@@ -1,14 +1,26 @@
 class Seat:
-    def __init__(self, free: bool, occupant: str) -> None:
+    def __init__(self, free: bool = True) -> None:
         self.free = free 
-        self.occupant = occupant
+        self.occupant = ""
     
-    # assign someone a seat if it's free
-    def set_occupant(name): 
-        pass
-    # remove someone from a seat and return the name of the person occupying the seat before
-    def remove_occupant():
-        pass
+# assign someone a seat if it's free
+    def set_occupant(self, name: str): 
+        # if it's free, put new student in the place
+        if self.free:
+            self.occupant = name
+            self.free = False
+        # if it's already occupied
+        else:
+            last_occupant = self.remove_occupant()
+            print (f"Bye bye {last_occupant}, they were removed from the seat.")
+            self.occupant = name
+            
+# remove someone from a seat and return the name of the person occupying the seat before
+    def remove_occupant(self):
+        # remove the person by making the seat free again        
+        self.free = True
+        # return the name of the last occupant
+        return self.occupant 
 
 class Table:
     def __init__(self, capacity: int) -> None:
