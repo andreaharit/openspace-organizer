714b30296f4326f62ac57849e938936daa48fe4b
Andrea Haritcalde <andrea.haritcalde@gmail.com>
2024-02-08 12:12:21 +0100

    Set up Table class

--- utils/table.py
+++ utils/table.py
@@ -1,39 +1,50 @@
 class Seat:
-    def __init__(self, free: bool = True) -> None:
+    def __init__(self, name, free: bool = True) -> None:
+        self.name = name
         self.free = free 
         self.occupant = ""
     
 # assign someone a seat if it's free
     def set_occupant(self, name: str): 
-        # if it's free, put new student in the place
         if self.free:
             self.occupant = name
-            self.free = False
-        # if it's already occupied
-        else:
-            last_occupant = self.remove_occupant()
-            print (f"Bye bye {last_occupant}, they were removed from the seat.")
-            self.occupant = name
-            
+            self.free = False        
+
 # remove someone from a seat and return the name of the person occupying the seat before
-    def remove_occupant(self):
-        # remove the person by making the seat free again        
+    def remove_occupant(self):   
         self.free = True
-        # return the name of the last occupant
         return self.occupant 
 
-class Table:
-    def __init__(self, capacity: int) -> None:
-        self.capacity = capacity
-        #list of Seat objects (size = capacity)        
-        self.seats = [] # To do
 
-    def has_free_spot():
-        # returns a boolean (True if a spot is available)
-        pass
-    def assign_seat(name):
-        # places someone at the table
-        pass
-    def left_capacity():
-        # returns an integer
-        pass
+class Table(Seat):
+    def __init__(self, name, capacity: int = 4) -> None:
+        self.name = name
+        self.capacity = capacity
+
+        # List of Seat objects (size = capacity)        
+        self.seats = [] 
+        for _ in range(self.capacity):
+            # Give names to the chairs to build a visual layout
+            seat_name = f"{self.name}.{_}"
+            self.seats.append(Seat(seat_name))
+
+    # Returns a boolean (True if a spot is available on the table)
+    def has_free_spot(self) -> bool:
+        for s in self.seats:
+            if s.free:
+                return True
+        return False
+    # Place someone at the table in a free spot
+    def assign_seat(self, name):        
+        for s in self.seats:
+            if s.free:
+                s.set_occupant(name)                               
+                break
+    # Return number of free seats at the table
+    def left_capacity(self) ->int :
+        count = 0
+        if self.has_free_spot():
+            for s in self.seats:
+                if s.free:
+                    count += 1
+        return count
